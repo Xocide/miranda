5b51a1bdc5f99b9847a094d1f3033f60e4ffdf37
Xocide <jack.polgar@gmail.com>
2010-09-17 12:52:34 +1000

    First commit

---
+++ _miranda/core.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Miranda
+ * Copyright (C) 2010 Jack Polgar
+ *
+ * This file is part of Miranda.
+ * 
+ * Miranda is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 only.
+ * 
+ * Miranda is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Miranda. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use \Miranda\Loader as Load;
+use \Miranda\Router as Router;
+
+// Load the core
+require("miranda.php");
+
+// Load the core classes
+require("libs/loader.php");
+require("libs/router.php");
+
+Router::route();
