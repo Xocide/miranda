fcc4791cb2832c4e92a34be3c612b93bb749e0f1
Xocide <jack.polgar@gmail.com>
2010-09-18 12:25:17 +1000

    jQuery included!

--- _app/views/layouts/default.php
+++ _app/views/layouts/default.php
@@ -2,53 +2,13 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<title>My App</title>
-		<style>
-		body {
-			font-family: Verdana, sans-serif;
-			font-size: 12px;
-			background: #042D3F;
-			color: #fff;
-			margin: 25px;
-			padding: 0;
-		}
-
-		#wrapper {
-			width: 500px;
-			margin: 0 auto;
-		}
-
-		#header {
-			background: #0A6793;
-			padding: 0 5px;
-		}
-		#header h1 {
-			margin: 0;
-		}
-		
-		#page {
-			background: #fff;
-			color: #000;
-		}
-		#page h2 {
-			margin-top: 0;
-			margin-bottom: 4px;
-		}
-		.content {
-			padding: 5px;
-		}
-
-		#footer {
-			background: #F6F6F6;
-			color: #000;
-			padding: 4px;
-			font-size: 10px;
-		}
-		</style>
+		<?php echo HTML::css_inc_tag(anchor('css/miranda.css')); ?>
+		<?php echo HTML::js_inc_tag(anchor('js/jquery.min.js')); ?>
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="header">
-				<h1>Miranda</h1>
+				<h1><?php echo HTML::link(anchor(),'Miranda'); ?></h1>
 			</div>
 			<div id="page" class="content">
 <?php echo $output; ?> 
--- _app/views/welcome/index.php
+++ _app/views/welcome/index.php
@@ -1,3 +1,20 @@
 <div align="center">
 	<h2>It works!</h2>
+	<?php echo HTML::link('http://github.com/Xocide/miranda','Miranda at GitHub'); ?>
 </div>
+<p align="center">
+	<a href="#" class="clickthis">Click this</a>
+</p>
+<div class="jquery" align="center" style="display:none;">
+	jQuery included!
+</div>
+<script type="text/javascript">
+$(function(){
+	$('.clickthis').click(function(){
+		$("div.jquery").show("slow");
+		$(this).hide();
+		return false;
+	});
+});
+</script>
+<!-- memory: {memory_useage} -->
